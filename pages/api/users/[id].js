ca4aec8610648a5d69a7e3e66268e350aa643ff0
Diego Serrano <diegoserranor@gmail.com>
2021-07-07 19:32:06 -0500

    Implement PUT method to update user data

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -22,16 +22,22 @@ export default async function handler(req, res) {
         res.status(400).json({ success: false });
       }
       break;
-    // case 'POST':
-    //   try {
-    //     const user = await User.create(
-    //       req.body
-    //     ); /* create a new model in the database */
-    //     res.status(201).json({ success: true, data: user });
-    //   } catch (error) {
-    //     res.status(400).json({ success: false });
-    //   }
-    //   break;
+    case 'PUT':
+      try {
+        const user = await User.findOneAndUpdate({ username: id }, req.body, {
+          new: true,
+          runValidators: true,
+        });
+        if (!user) {
+          return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({ user });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
     default:
       res.status(400).json({ success: false });
       break;
