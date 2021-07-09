6e1c41b87f044752b12a8c1222960a785a04d011
Diego Serrano <diegoserranor@gmail.com>
2021-07-08 23:32:53 -0500

    Protect admin page

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,10 +1,36 @@
+import Link from 'next/link';
+import { getSession, useSession } from 'next-auth/client';
 import Layout from '../../components/Layout/Layout';
 import styles from '../../styles/admin.module.css';
 
 export default function Admin() {
+  const [session, loading] = useSession();
+
+  // Do not show anything while we wait for the session
+  if (loading) {
+    return null;
+  }
+
+  // Refer the user to signing in again since they are not authorized
+  if (!loading && !session) {
+    return (
+      <div>
+        Try to{' '}
+        <Link href="/login">
+          <a style={{ color: '#ff8906', textDecoration: 'underline' }}>
+            log in again
+          </a>
+        </Link>
+        .
+      </div>
+    );
+  }
+
+  // Actual admin page components
   return (
     <Layout title="campfire">
       <h1 className={styles.headline}>admin</h1>
+      <p>welcome, {session.user.name}</p>
 
       <section>
         <h2 className={styles.subheadline}>links</h2>
@@ -21,11 +47,17 @@ export default function Admin() {
   );
 }
 
-// export async function getStaticProps(context) {
-//   const res = await fetch('http://localhost:3000/api/users/');
-//   const data = await res.json();
+export async function getServerSideProps(context) {
+  // Fetch from the server initially, then handle mutations on the client
+  const session = await getSession(context);
 
-//   return {
-//     props: { data }, // will be passed to the page component as props
-//   };
-// }
+  /**
+   * The session obtained from the context will be available in pageProps
+   * These then are passed to the next-auth Provider for the client
+   *
+   * See the setup in _app.js
+   */
+  return {
+    props: { session },
+  };
+}
