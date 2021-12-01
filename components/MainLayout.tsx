3d77e8deefc00df892579264786d766d13f4a810
Diego <diegoserranor@gmail.com>
2021-11-30 22:08:24 -0500

    Add footer to main layout

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,17 +1,22 @@
 import { Flex } from '@chakra-ui/react';
 
+import Footer from './Footer';
+
 export default function MainLayout({ children }) {
   return (
-    <Flex
-      w={{ base: '90%', md: '80%', xl: '1200px' }}
-      h="100vh"
-      pt="5vw"
-      pb="5vw"
-      m="0 auto"
-    >
-      <Flex flex={1} direction="column">
-        {children}
+    <Flex h="100vh" direction="column">
+      <Flex
+        w={{ base: '90%', md: '80%', xl: '1200px' }}
+        pt="5vw"
+        pb="5vw"
+        m="0 auto"
+        flex={1}
+      >
+        <Flex flex={1} direction="column">
+          {children}
+        </Flex>
       </Flex>
+      <Footer />
     </Flex>
   );
 }
